a2e81393f9a2ef5e202925c542672b16ba30a079
Hai Nguyen <haint2296@gmail.com>
2025-11-16 22:29:08 +0700

    Xay dung trang cac san pham nguoi dung

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -33,8 +34,8 @@ class MyApp extends StatelessWidget {
       title: 'MyShop',
       debugShowCheckedModeBanner: false,
       theme: themData,
-      home: SafeArea(
-        child: ProductsOverviewScreen(),
+      home: const SafeArea(
+        child: UserProductsScreen(),
       ),
     );
   }
